914a9465871ba252b5eb578834a8694f05be572f
EnascutaRazvan <enascutar@gmail.com>
2022-06-09 12:54:52 +0300

    Update UniveristyResources

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module com.example.mavenjavafx {
     requires com.fasterxml.jackson.databind;
     requires org.jsoup;
     requires org.jgrapht.core;
+    requires java.desktop;
 
     opens com.example.mavenjavafx to javafx.fxml, com.fasterxml.jackson.databind;
     exports com.example.mavenjavafx;
